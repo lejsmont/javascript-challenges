e8f95bfe1066b4c88eb811c7b87a4dee53b00fd9
Ludwik Ejsmont <ludwik.ejsmont@proton.me>
2023-05-04 14:26:28 +0200

    shorter version of the panic function

--- panic.js
+++ panic.js
@@ -2,18 +2,7 @@ function panic(text) {
     
     if (text.length <= 0) return
 
-    upperText = text.toUpperCase()
-
-    if (upperText.length === 1) return upperText
-    
-    const words = text.split(' ')
-
-    for (let i = 0; i < words.length-1;i++) {
-        words[i] = words[i] + ' 😱'
-    }
-
-    return words.join(' ')
-    
+    return text.toUpperCase().split(' ').join(' 😱 ') + '!'    
 
 }
 
