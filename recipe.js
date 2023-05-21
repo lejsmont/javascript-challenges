c5556367604b399d8d0a31f62d4b82316ded71d7
Ludwik Ejsmont <ludwik.ejsmont@proton.me>
2023-05-21 14:12:39 +0200

    recipes challenge resolved

---
+++ recipe.js
@@ -0,0 +1,44 @@
+/* Chef Mario's Recipe Book 
+Chef Mario was in the middle of writing his cookbook masterpiece
+when he spilled coffee on his keyboard! Now all his recipes have repeat
+ingredients.
+
+Help save Chef Mario's cookbook by writing a function that takes in an array 
+and returns a new array with all the duplicates removed. 
+
+Example input: ["🌈 rainbow", "🦄 unicorn", "🍭 lollipops", "🦄 unicorn", "🍭 lollipops"];
+Example output: ["🌈 rainbow", "🦄 unicorn", "🍭 lollipops"];
+*/ 
+
+const eggScrambleRecipe = [
+    "🥓 bacon",
+    "🥓 bacon", 
+    "🍳 eggs",
+    "🫑 green peppers",
+    "🧀 cheese",
+    "🌶️ hot sauce",
+    "🥓 bacon",
+    "🥦 broccoli", 
+    "🧀 cheese",
+    "🥦 broccoli", 
+    "🌶️ hot sauce"
+]
+
+function removeDupesFromArray(arr){
+    return [...new Set(arr)]
+}
+
+function removeDupesFromArray2(arr) {
+
+    let removedDupes = []
+
+    for (let i = 0 ; i < arr.length; i++){
+        if (!removedDupes.includes(arr[i])) {
+            removedDupes.push(arr[i])
+        }
+    }
+
+    return removedDupes
+}
+
+console.log(removeDupesFromArray2(eggScrambleRecipe));
