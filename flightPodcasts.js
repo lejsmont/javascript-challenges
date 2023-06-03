b8d672d225b9de21def6a2e9c360d116c6b8d5e4
Ludwik Ejsmont <ludwik.ejsmont@proton.me>
2023-06-03 14:57:43 +0200

    flight podcasts challenge resolved

---
+++ flightPodcasts.js
@@ -0,0 +1,36 @@
+import podcasts from "./podcasts.js";
+
+/* Welcome Aboard Scrimba Airlines 
+
+Our Scrimba Airlines in-flight entertainment package 
+includes a variety of podcasts. We need to add a feature that suggests
+podcasts to our patrons based on whether a flight is short or long. 
+
+Your sort function should take two arguments: the podcast data and
+flight length. If the flight is 60 minutes or less, sort the podcast list 
+from shortest to longest. If it's anything else, sort from longest
+to shortest. 
+
+Your function shouldn't return anything. Instead log a numbered list 
+of the title and duration of 
+each podcast to the console, like this:
+
+1. Crime Fan, 150 minutes
+2. Mythical Creatures, 99 minutes
+3. Crime Crime Crime, 70 minutes
+4. Coding Corner, 55 minutes
+5. Scrimba Podcast, 50 minutes
+6. Something about Witches, 35 minutes
+
+*/
+
+function sortByDuration(data, flightLength){
+  const orderedPodcasts = flightLength <= 60 ? data.sort((a,b)=> a.duration - b.duration) : data.sort((a,b)=> b.duration - a.duration)
+  
+    for (let i = 0; i<orderedPodcasts.length; i++) {
+        console.log(`${i+1}. ${orderedPodcasts[i].title}, ${orderedPodcasts[i].duration} minutes`)
+    }
+
+}
+
+sortByDuration(podcasts, 60);
